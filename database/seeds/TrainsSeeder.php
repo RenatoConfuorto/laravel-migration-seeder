658f8e10aac89744c74b426343df92493f5ff90a
RenatoConfuorto <renato.confuorto@outlook.it>
2022-07-02 12:13:17 +0200

    creazione tabella e inserimento dati con il seeder

--- database/seeds/TrainsSeeder.php
+++ database/seeds/TrainsSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainsSeeder extends Seeder
 {
@@ -9,8 +11,22 @@ class TrainsSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(Faker $faker)
+    {   
+        for($i = 0; $i < 50; $i++){
+            $train = new Train();
+            $train->azienda = $faker->word();
+            $train->stazione_di_partenza = $faker->city();
+            $train->stazione_di_arrivo = $faker->city();
+            $train->orario_di_partenza = $faker->time('H:i');
+            $train->orario_di_arrivo = $faker->time('H:i');
+            $train->codice_treno = $faker->unique()->randomNumber(9, true);
+            $train->numero_carrozze = $faker->randomNumber(2, false);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            
+            $train->save();
+        }
+
     }
 }
